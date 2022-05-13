597980e47d9712642e76904a6856c66636198f3f
Priyansh <priyanshg615@gmail.com>
2022-05-13 18:47:15 +0530

    Goodlives

--- src/Compoonents/Body/Home/home.js
+++ src/Compoonents/Body/Home/home.js
@@ -1,6 +1,6 @@
 import { React, useRef, useEffect } from "react";
-import "./home.css";
 import Lottie from "lottie-web";
+import "./home.css";
 import think from "../../../Animations/thinking-2.json";
 // import think2 from "../../../Animations/thinking-1.json";
 
